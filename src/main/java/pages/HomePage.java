bec965cc8b696b1a806ddfdbdf48c8e99c148deb
soumya0064 <soumyaranjan.sabat@gmail.com>
2021-04-24 19:19:15 +0530

    Second machine

--- src/main/java/pages/HomePage.java
+++ src/main/java/pages/HomePage.java
@@ -60,7 +60,7 @@ public void loginHome(String country,String name)
 	UserName.sendKeys(name);
 	driver.hideKeyboard();
 	LetsShopButton.click();
-	System.out.println("THIS IS A ADDITIONAL/Test LINE");
+	System.out.println("End of code");
 	
 }
 
